0100ef5c5392fd185fd1c746a454d948c4375a0b
chawlajay <chawlajay111@gmail.com>
2022-01-03 01:31:14 +0530

    added header, footer, about ejs files & work list

--- app.js
+++ app.js
@@ -9,22 +9,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var items = ["Welcome to TODO list","Enter task in box and click ADD Task"];
+var workItems = ["Welcome to Work TODO list"];
 app.get("/",function(req,res){
-    var today = new Date();
+    // var today = new Date();
 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    };
-    today = today.toLocaleDateString('hi-IN',options);
-    res.render('list',{day: today, listOfItems: items});
+    // var options = {
+    //     weekday: "long",
+    //     day: "numeric",
+    //     month: "long",
+    //     year: "numeric"
+    // };
+    // today = today.toLocaleDateString('hi-IN',options);
+    res.render('list',{listTitle: "Today", listOfItems: items});
+});
+
+app.get("/work",function(req,res){
+    res.render('list',{listTitle: "Work", listOfItems: workItems});
+});
+
+app.get("/about",function(req,res){
+    res.render("about");
 });
 
 app.post("/", function(req,res){
-    items.push(req.body.task);
-    res.redirect("/");
+    
+    let item = req.body.task;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
 app.listen(3000,function(){
