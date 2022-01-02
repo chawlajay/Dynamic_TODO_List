fdd10aae212a93b90ad28db68fb37461bf8203b1
chawlajay <chawlajay111@gmail.com>
2022-01-02 17:06:39 +0530

    added ejs library and views folder

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.send("Hello");
+    res.render('index');
 });
 
 app.listen(3000,function(){
