d44cacad53e75861c36ea57a1bc022286f09c47b
chawlajay <chawlajay111@gmail.com>
2022-01-02 20:39:37 +0530

    added form, post route, day template

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const ejs = require("ejs");
 const app = express();
 
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
-    res.render('index');
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    today = today.toLocaleDateString('hi-IN',options);
+    res.render('list',{day: today});
+});
+
+app.post("/", function(req,res){
+    console.log(req.body.task);
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
