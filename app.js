3a7b75b7e349de1bb2155ed4d062b4c782d5683b
chawlajay <chawlajay111@gmail.com>
2022-01-03 00:56:52 +0530

    added public/styles.css & styled list.ejs

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 var items = ["Welcome to TODO list","Enter task in box and click ADD Task"];
 app.get("/",function(req,res){
